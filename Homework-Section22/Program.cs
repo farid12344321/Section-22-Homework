39f385a79b50f7421c6968e763aa9332fe95605e
unknown <farid12344321>
2024-02-05 03:47:36 -0800

    homework

---
+++ Homework-Section22/Program.cs
@@ -0,0 +1,195 @@
+﻿
+
+
+
+
+using Homework_Section22;
+using System.Collections.Generic;
+
+List<Student> students = new List<Student>();
+
+
+
+string opt;
+do
+{
+
+    Console.WriteLine("1 Telebe alave et");
+    Console.WriteLine("2 Telebeye imtahan elave et");
+    Console.WriteLine("3 Telebenin 1 imtahan balina bax");
+    Console.WriteLine("4 Telebenin butun imtahanlarini goster");
+    Console.WriteLine("5 Telebenin imtahan ortalamasini goster");
+    Console.WriteLine("6 Telebeden imtahani sil");
+    Console.WriteLine("0 proqrami bitir");
+    Console.WriteLine("\nEmeliyati secin");
+    opt = Console.ReadLine();
+
+
+    switch (opt)
+    {
+        case "1":
+            Console.WriteLine("FullName elave et:");
+            string fullNameStr = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(fullNameStr))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("FullName bos ola bilmez");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            Student student = new Student()
+            {
+                FullName = fullNameStr,
+            };
+            students.Add(student);
+            break; 
+        case "2":
+            //2 seçilərsə tələbənin nömrəsi, imtahan adı və qiyməti daxil edilir
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Telebenin No daxil edin:");
+            string noStudentStr = Console.ReadLine();
+            int noStudent;
+            if (!int.TryParse(noStudentStr,out noStudent))
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Duzugn No daxil edin !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            Console.WriteLine("Imtahan adi :");
+            string examNameStr = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(examNameStr))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Imtahan adi bos ola bilmez !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            Console.WriteLine("Point daxil edin :");
+            string pointStr = Console.ReadLine();
+            double point;
+            if(!double.TryParse(pointStr, out point))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Duzugn Point daxil edin !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            foreach (var item in students)
+            {
+                if (noStudent == item.No)
+                {
+                    item.AddExam(examNameStr, point);
+                }
+            }
+            break;
+        case "3":
+            Console.WriteLine("Telebenin No daxil edin :");
+            string  noStuStr = Console.ReadLine();
+            int noStu;
+            if (!int.TryParse(noStuStr, out noStu))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Duzugn No daxil edin !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            Console.WriteLine("Imtahan adini daxil edin :");
+            string examNameStr2 = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(examNameStr2))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Imtahan adi bos ola bilmez !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            double examResult = students[noStu - 1].GetExamResult(examNameStr2);
+            Console.WriteLine("Balı: "+examResult);
+            break;
+        case "4":
+            //seçilərsə tələbənin no dəyəri daxil edilir və o tələbənin bütün imthan adları və balları göstərilir
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("No daxil edin: ");
+            string strNo = Console.ReadLine();
+            int no;
+            if (!int.TryParse(strNo,out no))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Duzugn No daxil edin !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            Student student1 = students[no - 1];
+            foreach (var item in student1.Exams)
+            {
+                Console.WriteLine("Imtahani :"+item.Key + " " +"Balı: "+ item.Value);
+            }
+            break; 
+        case "5":
+            // Tələbənin imtahan ortalamasını göstər
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("No daxil edin: ");
+            string strNo2 = Console.ReadLine();
+            int no2;
+            if (!int.TryParse(strNo2, out no2))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Duzugn No daxil edin !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            double examResult2 = students[no2 - 1].GetExamAvg();
+            Console.WriteLine("Ortalamasi: "+examResult2);
+            break;
+        case "6":
+            //seçilərsə tələbə no dəyəri və imtahan adı daxil edilir və həmin tələbədən həmin imtahan dəyəri silinir
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Telebenin No daxil edin :");
+            string noStuStr2 = Console.ReadLine();
+            int noStu2;
+            if (!int.TryParse(noStuStr2, out noStu2))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Duzugn No daxil edin !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            Console.WriteLine("Imtahan adini daxil edin :");
+            string examNameStr3 = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(examNameStr3))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Imtahan adi bos ola bilmez !");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+            Student student2 = students[noStu2 - 1];
+            foreach (var item in student2.Exams)
+            {
+                if (examNameStr3 == item.Key)
+                {
+                    student2.Exams.Remove(item.Key);
+                }
+            }
+            break;
+        case "0":
+            Console.WriteLine("Emeliyyat bitdi");
+            break;
+        default:
+            Console.WriteLine("Duzgun operator daxil edin");
+            break;
+    }
+} while (opt != "0");
---
+++ Homework-Section22/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Homework_Section22
+{
+    internal class Student
+    {
+
+        public Student()
+        {
+            _no++;
+            No = _no;
+        }
+        static int _no;
+        public int No { get; set; }
+        public string FullName { get; set; }
+
+        public Dictionary<string,double> Exams = new Dictionary<string,double>();
+        public override string ToString()
+        {
+            return $"{No} - {FullName}";
+        }
+        public void AddExam(string examName,double point)
+        {
+            Exams.Add(examName, point);
+        }
+        public double GetExamResult(string examName)
+        {
+            foreach (var item in Exams)
+            {
+                if (examName == item.Key)
+                {
+                    return item.Value;
+                }
+            }
+            return 0;
+        }
+        public double GetExamAvg()
+        {
+            int count = 0;
+            double sum = 0;
+            foreach (var item in Exams)
+            {
+                count++;
+                sum += item.Value;
+            }
+            return sum / count;
+        }
+    }
+}
